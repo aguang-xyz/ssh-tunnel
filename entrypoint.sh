fc3dd1a88c23221f75f61da7371a999064ebd524
Grey Wang <aguang.xyz@gmail.com>
2021-09-14 21:14:31 +1200

    Add keep alive option

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-ssh -R "0.0.0.0:${REMOTE_PORT}:localhost:${LOCAL_PORT}" -N ${REMOTE_USER}@${REMOTE_HOST}
+ssh -o ServerAliveInterval=60 -R "0.0.0.0:${REMOTE_PORT}:localhost:${LOCAL_PORT}" -N ${REMOTE_USER}@${REMOTE_HOST}
